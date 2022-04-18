8f1ac5dda652660a1b47af15bea70d08f9e8dd81
Serbest Ziyanak <serbest.ziyanak@gmail.com>
2022-04-18 16:00:04 +0300

    değişiklikler yapıldı

--- Reinforcement Learning Codes and Presentations/2) Q-Learning/gym_taxi_Qlearning-serbest.py
+++ Reinforcement Learning Codes and Presentations/2) Q-Learning/gym_taxi_Qlearning-serbest.py
@@ -43,13 +43,40 @@ for i in range(1,episode_number):
         next_state, reward, done, _ = env.step(action)
         
         # Q Learning Function
+        old_value = q_table[state, action]
+        next_max = np.max( q_table[next_state] )
+        
+        next_value = ( 1-alpha )*old_value + alpha * (reward + gamma * next_max)
         
         # Q table update
-        
+        q_table[ state, action ] = next_value
         # update state 
-        
+        state = next_state
         # find wrong dropouts
-        
+        if reward == -10:
+            dropouts +=1
+            
         if done:
             break
-        
+        reward_count += reward
+
+    if i%10 == 0:
+        dropout_list.append( dropouts )
+        reward_list.append( reward_count )
+        print( "Episode : {}, reward {}, wrong dropout {}".format(i, reward_count,dropouts) )
+
+# %% visualize
+fig,axs = plt.subplots( 1, 2 )
+axs[0].plot(reward_list)
+axs[0].set_xlabel("episode")
+axs[0].set_ylabel("reward")
+
+axs[1].plot(dropout_list)
+axs[1].set_xlabel("episode")
+axs[1].set_ylabel("dropouts")
+
+axs[0].grid(True)
+axs[1].grid(True)
+
+plt.show()
+
